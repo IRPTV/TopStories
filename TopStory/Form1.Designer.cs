238f30828defbe20ba6f85d15a1614a83d0bbd9b
mjpournassari <mjpournassari@gmail.com>
2014-12-14 13:26:06 +0330

    First Commit

---
+++ TopStory/Form1.Designer.cs
@@ -0,0 +1,191 @@
+﻿namespace TopStory
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.timer2 = new System.Windows.Forms.Timer(this.components);
+            this.timer3 = new System.Windows.Forms.Timer(this.components);
+            this.panel2.SuspendLayout();
+            this.tabControl1.SuspendLayout();
+            this.tabPage1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.tabPage2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.Color.White;
+            this.panel2.Controls.Add(this.tabControl1);
+            this.panel2.Controls.Add(this.button1);
+            this.panel2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel2.Location = new System.Drawing.Point(0, 0);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(299, 308);
+            this.panel2.TabIndex = 3;
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Controls.Add(this.tabPage1);
+            this.tabControl1.Controls.Add(this.tabPage2);
+            this.tabControl1.Location = new System.Drawing.Point(6, 41);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(290, 262);
+            this.tabControl1.TabIndex = 8;
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.Controls.Add(this.groupBox2);
+            this.tabPage1.Location = new System.Drawing.Point(4, 22);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage1.Size = new System.Drawing.Size(282, 236);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "LOG";
+            this.tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.richTextBox1);
+            this.groupBox2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.groupBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.groupBox2.Location = new System.Drawing.Point(3, 3);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(276, 230);
+            this.groupBox2.TabIndex = 6;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Log";
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(64)))));
+            this.richTextBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.richTextBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.richTextBox1.ForeColor = System.Drawing.Color.White;
+            this.richTextBox1.Location = new System.Drawing.Point(3, 16);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(270, 211);
+            this.richTextBox1.TabIndex = 4;
+            this.richTextBox1.Text = "";
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.Controls.Add(this.groupBox3);
+            this.tabPage2.Location = new System.Drawing.Point(4, 22);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage2.Size = new System.Drawing.Size(282, 236);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "WEB";
+            this.tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // groupBox3
+            // 
+            this.groupBox3.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.groupBox3.Enabled = false;
+            this.groupBox3.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(178)));
+            this.groupBox3.Location = new System.Drawing.Point(3, 3);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Size = new System.Drawing.Size(276, 230);
+            this.groupBox3.TabIndex = 7;
+            this.groupBox3.TabStop = false;
+            this.groupBox3.Text = "PREVIEW";
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.SteelBlue;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 13F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.ForeColor = System.Drawing.Color.White;
+            this.button1.Location = new System.Drawing.Point(6, 3);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(290, 32);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "Start";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // timer2
+            // 
+            this.timer2.Interval = 5000;
+            this.timer2.Tick += new System.EventHandler(this.timer2_Tick);
+            // 
+            // timer3
+            // 
+            this.timer3.Interval = 1000;
+            this.timer3.Tick += new System.EventHandler(this.timer3_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(299, 308);
+            this.Controls.Add(this.panel2);
+            this.Name = "Form1";
+            this.Text = "TopStory V1.5 20140130";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.panel2.ResumeLayout(false);
+            this.tabControl1.ResumeLayout(false);
+            this.tabPage1.ResumeLayout(false);
+            this.groupBox2.ResumeLayout(false);
+            this.tabPage2.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.GroupBox groupBox3;
+        private System.Windows.Forms.Timer timer2;
+        private System.Windows.Forms.Timer timer3;
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.TabPage tabPage2;
+    }
+}
+
---
+++ TopStory/Form1.cs
@@ -0,0 +1,238 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Configuration;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Windows.Forms;
+
+namespace TopStory
+{
+    public partial class Form1 : Form
+    {
+        int _ImagesCount = int.Parse(ConfigurationSettings.AppSettings["ImagesCount"].ToString().Trim());
+        int _ImageIndex = 1;
+        WebBrowser wb = new WebBrowser();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            button1.ForeColor = Color.White;
+            button1.Text = "Started";
+            button1.BackColor = Color.Red;
+
+            string[] FilesList = Directory.GetFiles(ConfigurationSettings.AppSettings["OutputPath"].ToString().Trim());
+            foreach (string item in FilesList)
+            {
+                try
+                {
+                    if (File.GetLastAccessTime(item) < DateTime.Now.AddHours(-48))
+                    {
+                        File.Delete(item);
+                        richTextBox1.Text += (item) + " *Deleted* \n";
+                        richTextBox1.SelectionStart = richTextBox1.Text.Length;
+                        richTextBox1.ScrollToCaret();
+                        Application.DoEvents();
+                    }
+                }
+                catch (Exception Exp)
+                {
+                    richTextBox1.Text += (Exp) + " \n";
+                    richTextBox1.SelectionStart = richTextBox1.Text.Length;
+                    richTextBox1.ScrollToCaret();
+                    Application.DoEvents();
+                }
+
+            }
+            GenerateScreenshot(ConfigurationSettings.AppSettings["WebUrl"].ToString().Trim());
+
+           
+        }
+        public void GenerateScreenshot(string url)
+        {
+            richTextBox1.Text = "";
+            timer1.Enabled = false;
+            timer2.Enabled = false;
+            timer3.Enabled = false;
+            wb.NewWindow += wb_NewWindow;
+            wb.ScrollBarsEnabled = false;
+            wb.ScriptErrorsSuppressed = true;
+            wb.Navigate(url);
+            while (wb.ReadyState != WebBrowserReadyState.Complete) { Application.DoEvents(); }
+            richTextBox1.Text += "Web Site Loaded" + " \n";
+            richTextBox1.SelectionStart = richTextBox1.Text.Length;
+            richTextBox1.ScrollToCaret();
+            Application.DoEvents();
+            groupBox3.Controls.Clear();
+            groupBox3.Controls.Add(wb);
+            wb.Width = 1010;
+            wb.Height = 700;
+            _ImageIndex = 1;
+            timer3.Enabled = true;
+        }
+        void wb_NewWindow(object sender, CancelEventArgs e)
+        {
+            e.Cancel = true;
+        }
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            int StartMinute = int.Parse(ConfigurationSettings.AppSettings["RenderIntervalMin"].ToString().Trim());
+            if (DateTime.Now.Minute >= StartMinute && DateTime.Now.Minute <= StartMinute + 1)
+            {
+                timer1.Enabled = false;
+                button1_Click(new object(), new EventArgs());
+            }
+
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            timer2.Interval = int.Parse(ConfigurationSettings.AppSettings["WaitForBetweenImagesSec"].ToString().Trim()) * 1000;
+          //  timer1.Interval = int.Parse(ConfigurationSettings.AppSettings["RenderIntervalMin"].ToString().Trim()) * 60 * 1000;
+            timer3.Interval = int.Parse(ConfigurationSettings.AppSettings["WaitForSiteLoadSec"].ToString().Trim()) * 1000;
+        }
+        private void timer2_Tick(object sender, EventArgs e)
+        {
+            timer2.Enabled = false;
+            if (_ImageIndex <= _ImagesCount)
+            {
+                Bitmap bitmap = new Bitmap(wb.Width, wb.Height);
+                wb.DrawToBitmap(bitmap, new Rectangle(0, 0, wb.Width, wb.Height));
+                Bitmap scaled = new Bitmap(1920, 1080);
+                using (Graphics graphics = Graphics.FromImage(scaled))
+                {
+                    graphics.DrawImage(bitmap, new Rectangle(0, 0, scaled.Width, scaled.Height));
+                }
+                scaled.Save(ConfigurationSettings.AppSettings["ImagePath"].ToString().Trim() + _ImageIndex.ToString() + ".png");
+                richTextBox1.Text += ConfigurationSettings.AppSettings["ImagePath"].ToString().Trim() + _ImageIndex.ToString() + ".png" + " \n";
+                richTextBox1.SelectionStart = richTextBox1.Text.Length;
+                richTextBox1.ScrollToCaret();
+                Application.DoEvents();
+
+                if (_ImageIndex == _ImagesCount)
+                {
+                    #region Renderer
+                    // D:\Program Files\Adobe\Adobe After Effects CS6\Support Files>aerender.exe -project "e:\AEProjs\iFILM website. 8 collect folder\Web Edit
+                    //22.8 collect.aep" -comp "web" -output  e:\2.avi
+
+                    Process proc = new Process();
+                    proc.StartInfo.FileName = "\"" + ConfigurationSettings.AppSettings["AeRenderPath"].ToString().Trim() + "\"";
+
+
+
+
+                    //string FilePath = dataGridView1.Rows[_Index].Cells[1].Value.ToString();
+
+                    //    string sss = Path.GetDirectoryName(FilePath.Replace("\\\\", "\\"));
+                    //    //    DirectoryInfo Dir = new DirectoryInfo);
+                    //    string[] Paths = FilePath.Replace(textBox1.Text.Trim(), "").Split('\\');
+                    //    string NewPath = "";
+                    //    for (int i = 0; i < Paths.Length - 1; i++)
+                    //    {
+                    //        NewPath += "\\" + Paths[i];
+                    //    }
+                    string DateTimeStr = string.Format("{0:0000}", DateTime.Now.Year) + "-" + string.Format("{0:00}", DateTime.Now.Month) + "-" + string.Format("{0:00}", DateTime.Now.Day) + "_" + string.Format("{0:00}", DateTime.Now.Hour) + "-" + string.Format("{0:00}", DateTime.Now.Minute) + "-" + string.Format("{0:00}", DateTime.Now.Second);
+
+                    DirectoryInfo Dir = new DirectoryInfo(ConfigurationSettings.AppSettings["OutputPath"].ToString().Trim());
+
+                    if (!Dir.Exists)
+                    {
+                        Dir.Create();
+                    }
+
+
+                    proc.StartInfo.Arguments = " -project " + "\"" + ConfigurationSettings.AppSettings["AeProjectFile"].ToString().Trim() + "\"" + "   -comp   \"" + ConfigurationSettings.AppSettings["Composition"].ToString().Trim() + "\" -output " + "\"" + ConfigurationSettings.AppSettings["OutputPath"].ToString().Trim() + ConfigurationSettings.AppSettings["OutPutFileName"].ToString().Trim() + "_" + DateTimeStr + ".mp4" + "\"";
+                    proc.StartInfo.RedirectStandardError = true;
+                    proc.StartInfo.UseShellExecute = false;
+                    proc.StartInfo.CreateNoWindow = true;
+                    proc.EnableRaisingEvents = true;
+                    proc.StartInfo.RedirectStandardOutput = true;
+                    proc.StartInfo.RedirectStandardError = true;
+
+                    if (!proc.Start())
+                    {
+
+
+                        return;
+                    }
+
+
+                    proc.PriorityClass = ProcessPriorityClass.Normal;
+                    StreamReader reader = proc.StandardOutput;
+                    string line;
+                    while ((line = reader.ReadLine()) != null)
+                    {
+                        richTextBox1.Text += (line) + " \n";
+                        richTextBox1.SelectionStart = richTextBox1.Text.Length;
+                        richTextBox1.ScrollToCaret();
+                        Application.DoEvents();
+                    }
+                    proc.Close();
+
+
+
+
+
+
+                    try
+                    {
+                        string StaticDestFileName = ConfigurationSettings.AppSettings["ScheduleDestFileName"].ToString().Trim();
+                        // File.Delete(StaticDestFileName);
+                        File.Copy(ConfigurationSettings.AppSettings["OutputPath"].ToString().Trim() + ConfigurationSettings.AppSettings["OutPutFileName"].ToString().Trim() + "_" + DateTimeStr + ".mp4", StaticDestFileName, true);
+                        richTextBox1.Text += "COPY FINAL:" + StaticDestFileName + " \n";
+                        richTextBox1.SelectionStart = richTextBox1.Text.Length;
+                        richTextBox1.ScrollToCaret();
+                        Application.DoEvents();
+                    }
+                    catch (Exception Ex)
+                    {
+                        richTextBox1.Text += Ex.Message + " \n";
+                        richTextBox1.SelectionStart = richTextBox1.Text.Length;
+                        richTextBox1.ScrollToCaret();
+                        Application.DoEvents();
+                    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+                    #endregion
+                    timer1.Enabled = true;
+                    timer2.Enabled = false;
+                    timer3.Enabled = false;
+                    this.Text = "TopStory V1.5 20140130::" + DateTime.Now.ToString();
+                    button1.ForeColor = Color.White;
+                    button1.Text = "Start";
+                    button1.BackColor = Color.Navy;
+
+                }
+                timer2.Enabled = true;
+                _ImageIndex++;
+            }
+        }
+        private void timer3_Tick(object sender, EventArgs e)
+        {
+            timer3.Enabled = false;
+            timer2_Tick(new Object(), new EventArgs());
+            timer2.Enabled = true;
+        }
+    }
+}
